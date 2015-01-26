603326216adcec1f75cab34996051193fe5d4a62
stormer <s2010@126.com>
2015-01-26 20:09:26 +0800

    ver1.0

---
+++ API/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace API
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
